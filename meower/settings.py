41d66e8526d6d6c5b06a772d3c2a5479e6428947
ElBrazilian <44048693+ElBrazilian@users.noreply.github.com>
2018-10-11 18:57:58 +0200

    update to key usage

--- meower/settings.py
+++ meower/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .key import key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yqr4ca@0-cnk(37mrb!vj)y)vfzt&*t32i7*clh+x-+=l9^+nh'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
